0f74021f89bf2e61e697602b36421f930a132d11
ZhouYQ1998 <471420883@qq.com>
2020-07-27 17:14:58 +0800

    MysqlLayerWriter & MysqlLayerWriterConfig

--- core/src/test/java/edu/zju/gis/hls/test/example/MysqlIOExample.java
+++ core/src/test/java/edu/zju/gis/hls/test/example/MysqlIOExample.java
@@ -5,8 +5,13 @@ import edu.zju.gis.hls.trajectory.analysis.rddLayer.LayerType;
 import edu.zju.gis.hls.trajectory.analysis.rddLayer.PolygonLayer;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.mysql.MysqlLayerReader;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.mysql.MysqlLayerReaderConfig;
+import edu.zju.gis.hls.trajectory.datastore.storage.writer.mysql.MysqlLayerWriter;
+import edu.zju.gis.hls.trajectory.datastore.storage.writer.mysql.MysqlLayerWriterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+import java.util.List;
 
 /**
  * @author Zhou
@@ -32,6 +37,9 @@ public class MysqlIOExample {
 
         Field fid = new Field("id", FieldType.ID_FIELD);
         Field shapeField = new Field("wkt", FieldType.SHAPE_FIELD);
+        // 除id，shape外，额外读取name字段
+        Field nameField = new Field("name", FieldType.NORMA_FIELD);
+        Field[] attributes = { nameField };
 
         // set up layer reader config
         MysqlLayerReaderConfig config = new MysqlLayerReaderConfig("mysql_layer_test1", source, LayerType.POLYGON_LAYER);
@@ -40,6 +48,8 @@ public class MysqlIOExample {
         config.setPassword(password);
         config.setIdField(fid);
         config.setShapeField(shapeField);
+        config.setAttributes(attributes);
+
 
         // read layer
         MysqlLayerReader<PolygonLayer> layerReader = new MysqlLayerReader<PolygonLayer>(ss, config);
@@ -49,6 +59,14 @@ public class MysqlIOExample {
         layer.makeSureCached();
         layer.count();
         log.info("Layer count: " + layer.count());
+
+        List<Tuple2<String, Polygon>> features = layer.collect();
+        features.forEach(x -> log.info(x._2.toJson()));
+
+        MysqlLayerWriterConfig wconfig = new MysqlLayerWriterConfig(source, "test2", user, password);
+        MysqlLayerWriter layerWriter = new MysqlLayerWriter(ss, wconfig);
+        layerWriter.write(layer);
+        log.info("Layer successfully writes!");
 
         layer.release();
 
