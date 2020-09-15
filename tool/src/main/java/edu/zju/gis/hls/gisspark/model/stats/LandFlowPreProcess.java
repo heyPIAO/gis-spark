2cbe79cbcc0f441ea97c54f4243e1a2e8ea0a57c
heyPIAO <11638008@zju.edu.cn>
2020-09-15 22:43:46 +0800

    二调数据预处理

--- tool/src/main/java/edu/zju/gis/hls/gisspark/model/stats/LandFlowPreProcess.java
+++ tool/src/main/java/edu/zju/gis/hls/gisspark/model/stats/LandFlowPreProcess.java
@@ -6,14 +6,18 @@ import edu.zju.gis.hls.trajectory.analysis.rddLayer.Layer;
 import edu.zju.gis.hls.trajectory.datastore.storage.LayerFactory;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.LayerReader;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.LayerReaderConfig;
-import edu.zju.gis.hls.trajectory.datastore.storage.writer.LayerWriter;
-import edu.zju.gis.hls.trajectory.datastore.storage.writer.LayerWriterConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+import java.util.List;
 
 /**
  * @author Hu
  * @date 2020/9/15
  * 二调数据关联生成merge表
  **/
+@Slf4j
 public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
 
   public LandFlowPreProcess(SparkSessionType type, String[] args) {
@@ -35,13 +39,29 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
     Layer xzdwLayer = xzdwReader.read();
     Layer dltbLayer = dltbReader.read();
 
+    Dataset<Row> lxdwRf = lxdwLayer.toDataset(this.ss);
+    lxdwRf.registerTempTable("lxdw");
+    Dataset<Row> xzdwRf = xzdwLayer.toDataset(this.ss);
+    xzdwRf.registerTempTable("xzdw");
+    Dataset<Row> dltbRf = dltbLayer.toDataset(this.ss);
+    dltbRf.registerTempTable("dltb");
+
     // TODO 根据字段关联
+    this.ss.sql("CREATE TABLE if not exists merges as \n" +
+      "(select d.bsm,d.zldwdm,d.tbbh, d.dlbm as tb_dlbm,d.tbmj as tb_dlmj,l.bsm as lx_id,l.dlbm as lx_dlbm,l.mj as lx_mj,\n" +
+      "x.bsm as xz_id,x.dlbm as xz_dlbm,x.cd as xz_cd,x.kd as xz_kd,d.wkt as tb_wkt,l.wkt as lx_wkt,\n" +
+      "x,wkt as xz_wkt,d.tkxs as tb_tkcs,x.kcbl as xz_kcbl\n" +
+      "from \"dltb\" as d \n" +
+      "LEFT JOIN \"lxdw\" as l on d.zldwdm=l.zldwdm and d.tbbh=l.zltbbh\n" +
+      "LEFT JOIN \"xzdw\" as x on \n" +
+      "(d.zldwdm=x.kctbdwdm1 and d.tbbh=x.kctbbh1) or (d.zldwdm=x.kctbdwdm2 and d.tbbh=x.kctbbh2))");
 
-
-
-    LayerWriterConfig writerConfig = LayerFactory.getWriterConfig(this.arg.getWriterConfig());
-    LayerWriter writer = LayerFactory.getWriter(this.ss, writerConfig);
-    // writer.write();
+    Dataset<Row> mergeResult = this.ss.table("merges");
+    List<Row> r = mergeResult.collectAsList();
+    log.info(String.valueOf(r.size()));
+//    LayerWriterConfig writerConfig = LayerFactory.getWriterConfig(this.arg.getWriterConfig());
+//    LayerWriter writer = LayerFactory.getWriter(this.ss, writerConfig);
+//    writer.write(dltbLayer);
   }
 
 
@@ -49,4 +69,5 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
     LandFlowPreProcess preProcess = new LandFlowPreProcess(SparkSessionType.LOCAL, args);
     preProcess.exec();
   }
+
 }
