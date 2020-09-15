c7675f2ec8c73571b59e73c8370a78d25e8d7b63
heyPIAO <11638008@zju.edu.cn>
2020-09-15 22:08:01 +0800

    平方米，平方千米，亩，三种面积下的平差

--- core/src/main/java/edu/zju/gis/hls/trajectory/analysis/rddLayer/Layer.java
+++ core/src/main/java/edu/zju/gis/hls/trajectory/analysis/rddLayer/Layer.java
@@ -60,12 +60,12 @@ public class Layer<K, V extends Feature> extends JavaPairRDD<K, V> implements Se
                 scala.reflect.ClassTag$.MODULE$.apply(f));
     }
 
-    protected Layer(RDD<Tuple2<K, V>> rdd) {
+    public Layer(RDD<Tuple2<K, V>> rdd) {
         super(rdd, scala.reflect.ClassTag$.MODULE$.apply(String.class), scala.reflect.ClassTag$.MODULE$.apply(Feature.class));
         this.metadata = new LayerMetadata();
     }
 
-    protected Layer(RDD<Tuple2<K, V>> rdd, ClassTag<K> kClassTag, ClassTag<V> vClassTag) {
+    public Layer(RDD<Tuple2<K, V>> rdd, ClassTag<K> kClassTag, ClassTag<V> vClassTag) {
         super(rdd, kClassTag, vClassTag);
         this.metadata = new LayerMetadata();
     }
