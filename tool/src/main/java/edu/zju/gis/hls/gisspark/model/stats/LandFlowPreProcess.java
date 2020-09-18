6515c912e3b9d507cf3b7646844fa08524cf8eaa
moral <469304162@qq.com>
2020-09-18 13:15:37 +0800

    与xzdw2连接时判断是否已经与xzdw1关联，fliter多余项再合并

--- tool/src/main/java/edu/zju/gis/hls/gisspark/model/stats/LandFlowPreProcess.java
+++ tool/src/main/java/edu/zju/gis/hls/gisspark/model/stats/LandFlowPreProcess.java
@@ -4,7 +4,10 @@ import edu.zju.gis.hls.gisspark.model.BaseModel;
 import edu.zju.gis.hls.gisspark.model.util.SparkSessionType;
 import edu.zju.gis.hls.trajectory.analysis.model.Feature;
 import edu.zju.gis.hls.trajectory.analysis.model.Field;
+import edu.zju.gis.hls.trajectory.analysis.model.FieldType;
+import edu.zju.gis.hls.trajectory.analysis.model.Term;
 import edu.zju.gis.hls.trajectory.analysis.rddLayer.Layer;
+import edu.zju.gis.hls.trajectory.analysis.rddLayer.StatLayer;
 import edu.zju.gis.hls.trajectory.datastore.storage.LayerFactory;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.LayerReader;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.LayerReaderConfig;
@@ -14,12 +17,20 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.Optional;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.catalyst.expressions.GenericRow;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Hu
@@ -58,6 +69,7 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
                 return new Tuple2<>(StringUtils.join(kf, "##"), o._2);
             }
         });
+
 
         JavaPairRDD<String, Feature> lxdwLayer2 = lxdwLayer.mapToPair(new PairFunction<Tuple2<String, Feature>, String, Feature>() {
             @Override
@@ -114,8 +126,9 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
             }
         });
 
+        JavaPairRDD<String, Tuple2<Feature, Optional<Feature>>> dltb_xzdw_1 = l1.leftOuterJoin(xzdwLayer1);
 
-        l1 = l1.leftOuterJoin(xzdwLayer1).mapToPair(new PairFunction<Tuple2<String, Tuple2<Feature, Optional<Feature>>>, String, Feature>() {
+        JavaPairRDD<String, Feature> l2 = dltb_xzdw_1.mapToPair(new PairFunction<Tuple2<String, Tuple2<Feature, Optional<Feature>>>, String, Feature>() {
             @Override
             public Tuple2<String, Feature> call(Tuple2<String, Tuple2<Feature, Optional<Feature>>> in) throws Exception {
                 Feature f = new Feature(in._2._1);
@@ -136,16 +149,36 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
                 f.addAttribute(kdF, xz.getAttribute("KD"));
                 f.addAttribute(wktF, xz.getAttribute("WKT"));
                 f.addAttribute(kcblF, xz.getAttribute("KCBL"));
+
+                Feature lx = in._2._1;
+                Field lx_id = new Field("lx_id");
+                Field lx_dlbm = new Field("lx_dlbm");
+                Field lx_mj = new Field("lx_mj");
+                lx_mj.setType(Double.class);
+                Field lx_wkt = new Field("lx_wkt");
+                f.addAttribute(lx_id, lx.getAttribute("BSM"));
+                f.addAttribute(lx_dlbm, lx.getAttribute("DLBM"));
+                f.addAttribute(lx_mj, lx.getAttribute("MJ"));
+                f.addAttribute(lx_wkt, lx.getAttribute("WKT"));
+
+                Field tb_wkt = new Field("tb_wkt");
+                f.addAttribute(tb_wkt, in._2._1.getGeometryWkt());
+
                 return new Tuple2<>(in._1, f);
             }
-        }).leftOuterJoin(xzdwLayer2).mapToPair(new PairFunction<Tuple2<String, Tuple2<Feature, Optional<Feature>>>, String, Feature>() {
+        });
+
+        JavaPairRDD<String, Tuple2<Feature, Optional<Feature>>> dltb_xzdw_2 = l2.leftOuterJoin(xzdwLayer2);
+
+        JavaPairRDD<String, Feature> l3 = dltb_xzdw_2.mapToPair(new PairFunction<Tuple2<String, Tuple2<Feature, Optional<Feature>>>, String, Feature>() {
             @Override
             public Tuple2<String, Feature> call(Tuple2<String, Tuple2<Feature, Optional<Feature>>> in) throws Exception {
                 Feature f = new Feature(in._2._1);
                 if (!in._2._2.isPresent())
                     return new Tuple2<>(in._1, in._2._1);
-                if (in._2._1.getAttribute("xz_id") != null)
-                    return new Tuple2<>(in._1, in._2._1);
+                if (in._2._1.getAttribute("xz_id") != null) {
+                    return new Tuple2<>("EMPTY", null);
+                }
                 Feature xz = in._2._2.get();
                 Field bsmF = new Field("xz_id");
                 Field dlbmF = new Field("xz_dlbm");
@@ -162,50 +195,110 @@ public class LandFlowPreProcess extends BaseModel<LandFlowPreProcessArgs> {
                 f.addAttribute(kdF, xz.getAttribute("KD"));
                 f.addAttribute(wktF, xz.getAttribute("WKT"));
                 f.addAttribute(kcblF, xz.getAttribute("KCBL"));
+
+                Feature lx = in._2._1;
+                Field lx_id = new Field("lx_id");
+                Field lx_dlbm = new Field("lx_dlbm");
+                Field lx_mj = new Field("lx_mj");
+                lx_mj.setType(Double.class);
+                Field lx_wkt = new Field("lx_wkt");
+                f.addAttribute(lx_id, lx.getAttribute("BSM"));
+                f.addAttribute(lx_dlbm, lx.getAttribute("DLBM"));
+                f.addAttribute(lx_mj, lx.getAttribute("MJ"));
+                f.addAttribute(lx_wkt, lx.getAttribute("WKT"));
+
+                Field tb_wkt = new Field("tb_wkt");
+                f.addAttribute(tb_wkt, in._2._1.getGeometryWkt());
+
                 return new Tuple2<>(in._1, f);
             }
-        });
+        }).filter(x -> !x._1.equals("EMPTY"));
 
-//        l1.cache();
-//        Feature f = l1.first()._2;
+        JavaPairRDD<String, Feature> unionLayer = l2.union(l3);
 
-        l1.saveAsTextFile("/Users/moral/Desktop/result/");
+        unionLayer.cache();
+        Feature f = unionLayer.first()._2;
+        log.info("catch");
+
+//        unionLayer.saveAsTextFile("/Users/moral/Desktop/result/");
+
+        JavaRDD<Row> rdd = unionLayer.map(x -> x._2).map(x -> transform(x));
+        StructType st = this.getLayerStructType();
+        Dataset<Row> df = this.ss.createDataFrame(rdd, st);
+        df.cache();
+        df.printSchema();
+        df.write()
+                .format("jdbc")
+                .option("url", "jdbc:postgresql://10.79.231.83:5430/template_postgis")
+                .option("dbtable", String.format("%s.%s", "public", "merge_spark"))
+                .option("user", "postgres")
+                .option("password", "postgres")
+                .mode(SaveMode.Overwrite)
+                .save();
+        df.unpersist();
 
         log.info(String.valueOf(l1.count()));
+    }
+    private StructType getLayerStructType() {
+        List<StructField> sfsl = new LinkedList<>();
 
-        // TODO 写出
-//        Dataset<Row> lxdwRf = lxdwLayer.toDataset(this.ss);
-//        lxdwRf.registerTempTable("lxdw");
-//        Dataset<Row> xzdwRf = xzdwLayer.toDataset(this.ss);
-//        xzdwRf.registerTempTable("xzdw");
-//        Dataset<Row> dltbRf = dltbLayer.toDataset(this.ss);
-//        dltbRf.registerTempTable("dltb");
-//    LayerWriterConfig writerConfig = LayerFactory.getWriterConfig(this.arg.getWriterConfig());
-//    LayerWriter writer = LayerFactory.getWriter(this.ss, writerConfig);
-//    writer.write(dltbLayer);
+        Field BSM = new Field("BSM");
+        sfsl.add(convertFieldToStructField(BSM));
+        Field ZLDWDM = new Field("ZLDWDM");
+        sfsl.add(convertFieldToStructField(ZLDWDM));
+        Field TBBH = new Field("TBBH");
+        sfsl.add(convertFieldToStructField(TBBH));
+        Field DLBM = new Field("DLBM");
+        sfsl.add(convertFieldToStructField(DLBM));
+        Field TBMJ = new Field("TBMJ");
+        sfsl.add(convertFieldToStructField(TBMJ));
+        Field TKXS = new Field("TKXS");
+        sfsl.add(convertFieldToStructField(TKXS));
+        Field tb_wkt = new Field("tb_wkt");
+        sfsl.add(convertFieldToStructField(tb_wkt));
 
-//        this.ss.sql("DROP TABLE IF EXISTS merges");
-//        String createMergesTable =
-//                "CREATE TABLE merges as " +
-//                        "(select d.bsm,d.zldwdm,d.tbbh, d.dlbm as tb_dlbm,d.tbmj as tb_dlmj,l.bsm as lx_id,l.dlbm as lx_dlbm,l.mj as lx_mj," +
-//                        "x.bsm as xz_id,x.dlbm as xz_dlbm,x.cd as xz_cd,x.kd as xz_kd, d.wkt as tb_wkt, l.wkt as lx_wkt," +
-//                        "x.wkt as xz_wkt,d.tkxs as tb_tkcs,x.kcbl as xz_kcbl " +
-//                        "from dltb as d " +
-//                        "LEFT JOIN lxdw as l on d.zldwdm=l.zldwdm and d.tbbh=l.zltbbh " +
-//                        "LEFT JOIN xzdw as x on " +
-//                        "(d.zldwdm=x.kctbdwdm1 and d.tbbh=x.kctbbh1) or (d.zldwdm=x.kctbdwdm2 and d.tbbh=x.kctbbh2))";
-//        String createMergesTable =
-//                "CREATE TABLE merges as (select bsm from dltb)";
-//        Dataset<Row> re = this.ss.sql(createMergesTable);
-//        log.info(re.schema().treeString());
-//
-//
-//
-//        Dataset<Row> mergeResult = this.ss.table("merges");
-//        List<Row> r = mergeResult.collectAsList();
-//        log.info(String.valueOf(r.size()));
+        Field xz_id = new Field("xz_id");
+        sfsl.add(convertFieldToStructField(xz_id));
+        Field xz_dlbm = new Field("xz_dlbm");
+        sfsl.add(convertFieldToStructField(xz_dlbm));
+        Field xz_cd = new Field("xz_cd");
+//        xz_cd.setType(Double.class);
+        sfsl.add(convertFieldToStructField(xz_cd));
+        Field xz_kd = new Field("xz_kd");
+//        xz_kd.setType(Double.class);
+        sfsl.add(convertFieldToStructField(xz_kd));
+        Field xz_wkt = new Field("xz_wkt");
+        sfsl.add(convertFieldToStructField(xz_wkt));
+        Field xz_kcbl = new Field("xz_kcbl");
+//        xz_kcbl.setType(Double.class);
+        sfsl.add(convertFieldToStructField(xz_kcbl));
+
+        Field lx_id = new Field("lx_id");
+        sfsl.add(convertFieldToStructField(lx_id));
+        Field lx_dlbm = new Field("lx_dlbm");
+        sfsl.add(convertFieldToStructField(lx_dlbm));
+        Field lx_mj = new Field("lx_mj");
+//        lx_mj.setType(Double.class);
+        sfsl.add(convertFieldToStructField(lx_mj));
+        Field lx_wkt = new Field("lx_wkt");
+        sfsl.add(convertFieldToStructField(lx_wkt));
+
+        StructField[] sfs = new StructField[sfsl.size()];
+        sfsl.toArray(sfs);
+        return new StructType(sfs);
     }
 
+    public Row transform(Feature feature) {
+        Row row = new GenericRow(feature.toObjectArray());
+        return row;
+    }
+
+    private StructField convertFieldToStructField(Field field) {
+        return new StructField(field.getName(),
+                Field.converFieldTypeToDataType(field),
+                !(field.getFieldType().equals(FieldType.ID_FIELD) || field.getFieldType().equals(FieldType.SHAPE_FIELD)),
+                Metadata.empty());
+    }
 
     public static void main(String[] args) throws Exception {
         LandFlowPreProcess preProcess = new LandFlowPreProcess(SparkSessionType.LOCAL, args);
