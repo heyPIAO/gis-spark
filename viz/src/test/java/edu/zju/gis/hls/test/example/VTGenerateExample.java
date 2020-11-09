556a74cc4dcdb89e4e10bdf85b6a932bb46018d5
ZhouYQ1998 <471420883@qq.com>
2020-11-09 21:36:07 +0800

    optimize VTGenerateExample.

--- viz/src/test/java/edu/zju/gis/hls/test/example/VTGenerateExample.java
+++ viz/src/test/java/edu/zju/gis/hls/test/example/VTGenerateExample.java
@@ -5,13 +5,21 @@ import edu.zju.gis.hls.trajectory.analysis.index.DistributeSpatialIndex;
 import edu.zju.gis.hls.trajectory.analysis.index.IndexType;
 import edu.zju.gis.hls.trajectory.analysis.index.SpatialIndexFactory;
 import edu.zju.gis.hls.trajectory.analysis.index.unifromGrid.UniformGridIndexConfig;
+import edu.zju.gis.hls.trajectory.analysis.model.MultiPolygon;
+import edu.zju.gis.hls.trajectory.analysis.model.Term;
 import edu.zju.gis.hls.trajectory.analysis.rddLayer.KeyIndexedLayer;
 import edu.zju.gis.hls.trajectory.analysis.rddLayer.LayerType;
 import edu.zju.gis.hls.trajectory.analysis.rddLayer.MultiPolygonLayer;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.shp.ShpLayerReader;
 import edu.zju.gis.hls.trajectory.datastore.storage.reader.shp.ShpLayerReaderConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.SparkSession;
+import org.geotools.referencing.CRS;
+import scala.Tuple2;
+
+import java.io.File;
+import java.io.FileReader;
 
 /**
  * @author Zhou
@@ -28,7 +36,7 @@ public class VTGenerateExample {
         int zMax = 12;
         String fileType = "mvt";
         String layerName = "LCRA";
-        String source = "E:\\Data\\shp\\LCRA.shp";
+        String source = "E:\\Data\\shp\\LCRA_4326.shp";
         String outDir = "E:\\Data\\vectorTileMVT";
 
         // Setup SparkSession
@@ -38,14 +46,37 @@ public class VTGenerateExample {
                 .master("local[*]")
                 .getOrCreate();
 
+        String rawCrs = "WKT_3857";
+        File file = new File(source.replace(".shp", ".prj"));
+        FileReader fr = new FileReader(file);
+        char[] buffer = new char[6];
+        if(fr.read(buffer) == 6){
+            if(String.valueOf(buffer).equals("GEOGCS")) rawCrs = "WKT_4326";
+        }
+
         // Read Kecheng data from shp
         MultiPolygonLayer layer = shpLayerRead(ss, layerName, source);
+
+        if(rawCrs.equals("WKT_4326")){
+            layer = (MultiPolygonLayer) layer.mapToLayer(new Function<Tuple2<String, MultiPolygon>, Tuple2<String, MultiPolygon>>() {
+                @Override
+                public Tuple2<String, MultiPolygon> call(Tuple2<String, MultiPolygon> t) throws Exception {
+                    t._2.transform(CRS.parseWKT(Term.WKT_4326), CRS.parseWKT(Term.WKT_3857));
+                    return t;
+                }
+            });
+        }
+
+        layer.getMetadata().setCrs(CRS.parseWKT(Term.WKT_3857));
         layer.cache();
         log.info(String.format("%s has %d partitions.", layerName, layer.count()));
 
         // Setup KeyIndexedLayer
         DistributeSpatialIndex si = SpatialIndexFactory.getDistributedSpatialIndex(IndexType.UNIFORM_GRID, new UniformGridIndexConfig(zMax));
         KeyIndexedLayer<MultiPolygonLayer> keyLayer = si.index(layer);
+
+        // Show sample
+        keyLayer.getLayer().rdd().toJavaRDD().map(x -> x._1).take(10).forEach(System.out::println);
 
         // Generate vectorTile
         VectorTileGenerator vtGenerator = new VectorTileGenerator(zMin, zMax , fileType);
